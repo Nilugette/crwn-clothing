78e644137a70872d0666588976cb48dc238c264a
Nilugette <nilugette@yahoo.fr>
2021-03-17 19:46:45 +0100

    Memoized selectors

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React from 'react'
+import { connect } from 'react-redux'
 
-import CustomButton from '../custom-button/custom-button.component';
-import CartItem from '../cart-item/cart-item.component';
+import CustomButton from '../custom-button/custom-button.component'
+import CartItem from '../cart-item/cart-item.component'
+import { selectCartItems } from '../../redux/cart/cart.selectors';
 
-import './cart-dropdown.styles.scss';
+import './cart-dropdown.styles.scss'
 
 const CartDropdown = ({ cartItems }) => {
     return (
@@ -19,10 +20,10 @@ const CartDropdown = ({ cartItems }) => {
       )
 }
 
-const mapStateToProps = ({ cart: { cartItems } }) => {
+const mapStateToProps = state => {
     return { 
-        cartItems
+        cartItems: selectCartItems(state)
     } 
 }
   
-export default connect(mapStateToProps)(CartDropdown);
+export default connect(mapStateToProps)(CartDropdown)
--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,29 +1,36 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React from 'react'
+import { connect } from 'react-redux'
 
-import { toggleCartHidden } from '../../redux/cart/cart.actions.js';
+import { toggleCartHidden } from '../../redux/cart/cart.actions.js'
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors.js'
 
-import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
+import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg'
 
-import './cart-icon.styles.scss';
+import './cart-icon.styles.scss'
 
-const CartIcon = ({ toggleCartHidden }) => {
+const CartIcon = ({ toggleCartHidden, itemCount  }) => {
     return (
         <div className='cart-icon' onClick={toggleCartHidden}>
           <ShoppingIcon className='shopping-icon' />
-          <span className='item-count'>0</span>
+          <span className='item-count'>{itemCount}</span>
         </div>
-      );
+      )
 }
 
 const mapDispatchToProps = dispatch => {
     return {
         toggleCartHidden: () => dispatch(toggleCartHidden())
     }
-  }
+}
+
+const mapStateToProps = state => {
+    return {
+        itemCount: selectCartItemsCount(state)
+    }
+}
   
 
 export default connect(
-  null,
-  mapDispatchToProps
+    mapStateToProps,
+    mapDispatchToProps
 )(CartIcon);
